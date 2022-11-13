c1964c6e02bb07ebcac34fcdf9de75c2c50db64a
John Borys <john_borys@comcast.net>
2022-11-13 14:34:55 -0500

    Test drove this out to a messy solution. All tests pass, but needs to be refactored. Should we add a sellgumball()? Should we add 1 gumball to every refill? Right now, we start with zero gumballs. Shouldn't that fail some tests???

--- src/main/java/edu/designpatterns/state/GumballMachine.java
+++ src/main/java/edu/designpatterns/state/GumballMachine.java
@@ -1,22 +1,61 @@
 package edu.designpatterns.state;
 
+import static edu.designpatterns.state.Messages.*;
+
 public class GumballMachine {
-	private GumballHardwareDevice device;
+	private final GumballHardwareDevice device;
+	private boolean soldOut;
+	private boolean hasQuarter;
 
 	public GumballMachine(GumballHardwareDevice device) {
 		this.device = device;
-		device.displayMessage(Messages.SO_START);
+		device.displayMessage(SO_START);
+		soldOut = true;
 	}
 
 	public void quarterInserted() {
+		if(soldOut) {
+			device.dispenseQuarter();
+			device.displayMessage(SO_QUART);
+		} else if (hasQuarter) {
+			device.dispenseQuarter();
+			device.displayMessage(HQ_QUART);
+		} else {
+			device.displayMessage(NQ_QUART);
+			hasQuarter = true;
+		}
 	}
 
 	public void ejectQuarterRequested() {
+		if (soldOut) {
+			device.displayMessage(SO_EJECT);
+		} else if (hasQuarter) {
+			device.dispenseQuarter();
+			device.displayMessage(HQ_EJECT);
+		} else {
+			device.displayMessage(NQ_EJECT);
+		}
 	}
 
 	public void crankTurned() {
+		if (soldOut) {
+			device.displayMessage(SO_CRANK);
+		} else if (hasQuarter) {
+			if (device.dispenseGumball()) {
+				device.displayMessage(NQ_START);
+			} else {
+				soldOut = true;
+				device.displayMessage(SO_START);
+				device.dispenseQuarter();
+			}
+			hasQuarter = false;
+		} else {
+			device.displayMessage(NQ_CRANK);
+		}
 	}
 
 	public void reset() {
+		device.displayMessage(NQ_START);
+		soldOut = false;
 	}
 }
--- src/test/java/edu/designpatterns/state/GumballMachineTest.java
+++ src/test/java/edu/designpatterns/state/GumballMachineTest.java
@@ -1,26 +1,151 @@
 package edu.designpatterns.state;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GumballMachineTest {
-	private TestDevice device = new TestDevice();
-	private GumballMachine gumballMachine;
+    private final TestDevice device = new TestDevice();
+    private GumballMachine gumballMachine;
 
-	@BeforeEach
-	public void setup() {
-		gumballMachine = new GumballMachine(device);
-	}
+    @BeforeEach
+    public void setup() {
+        gumballMachine = new GumballMachine(device);
+    }
 
-	@Test
-	public void initialConditionsEmptyMachineShouldShowSO_StartMessage() {
-		assertThat(device.getDisplayedMessage()).isEqualTo(Messages.SO_START);
-	}
+    @Nested
+    @DisplayName("Initial Condition")
+    class InitialCondition {
 
-	@Test
-	public void initialConditionsEmptyMachineShouldNotDispenseQuarter() {
-		assertThat(device.wasQuarterEjected()).isFalse();
-	}
+        @Test
+        public void initialConditionsEmptyMachineShouldNotDispenseQuarter() {
+            assertThat(device.wasQuarterEjected()).isFalse();
+        }
+
+        @Test
+        public void initialConditionsEmptyMachineShouldShowSO_StartMessage() {
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.SO_START);
+        }
+    }
+
+    @Nested
+    @DisplayName("Sold Out")
+    class SoldOut {
+        @Test
+        @DisplayName("Has no Gumballs")
+        public void quarterInsertedMachineSoldOutShouldReleaseQuarter() {
+            gumballMachine.quarterInserted();
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.SO_QUART);
+            assertThat(device.wasQuarterEjected()).isTrue();
+        }
+
+        @Test
+        public void pushEjectButtonMachineSoldOutShouldDisplayEjectMsg() {
+            gumballMachine.ejectQuarterRequested();
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.SO_EJECT);
+        }
+
+        @Test
+        public void turnCrankMachineSoldOutShouldDisplayNoGumballMsg() {
+            gumballMachine.crankTurned();
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.SO_CRANK);
+        }
+
+        @Test
+        public void resetMachineSoldOutShouldDisplayNoQuarterStartMsg() {
+            gumballMachine.reset();
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.NQ_START);
+        }
+
+    }
+
+    @Nested
+    @DisplayName("Has No Quarter")
+    class HasNoQuarter {
+        @Test
+        public void quarterInsertedShouldDisplayTurnCrankMsg() {
+            gumballMachine.reset();
+            gumballMachine.quarterInserted();
+
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.NQ_QUART);
+        }
+
+        @Test
+        public void ejectQuarterPressedShouldDisplayNoQuarterEjectMsg() {
+            gumballMachine.reset();
+            gumballMachine.ejectQuarterRequested();
+
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.NQ_EJECT);
+        }
+
+        @Test
+        public void turnCrankWithouQuarterShouldDisplayCrankMsg() {
+            gumballMachine.reset();
+            gumballMachine.crankTurned();
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.NQ_CRANK);
+        }
+    }
+
+    @Nested
+    @DisplayName("Gumball Machine Has Quarter")
+    class HasQuarter {
+        @Test
+        public void shouldDisplayTurnCrankForAGumballMsg() {
+            gumballMachine.reset();
+            gumballMachine.quarterInserted();
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.HQ_START);
+        }
+
+        @Test
+        public void quarterInsertedShouldDisplayInsertedMsgAndDispenseQuarter() {
+            gumballMachine.reset();
+            gumballMachine.quarterInserted();
+            gumballMachine.quarterInserted();
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.HQ_QUART);
+            assertThat(device.wasQuarterEjected()).isTrue();
+        }
+
+        @Test
+        public void quarterEjectedShouldDisplayEjectedQuarterMsg() {
+            gumballMachine.reset();
+            gumballMachine.quarterInserted();
+            gumballMachine.ejectQuarterRequested();
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.HQ_EJECT);
+        }
+
+        @Test
+        public void crankTurnedShouldDisplayGumballMsgAndDispenseGumball() {
+            gumballMachine.reset();
+            device.addGumballs(5);
+            gumballMachine.quarterInserted();
+            gumballMachine.crankTurned();
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.HQ_CRANK);
+            assertThat(device.dispenseGumball()).isTrue();
+        }
+
+        @Test
+        public void crankTurnedNoGumballsLeftShouldDispenseQuarterAndDisplaySoldOutMsg() {
+            gumballMachine.reset();
+            gumballMachine.quarterInserted();
+            gumballMachine.crankTurned();
+
+
+            assertThat(device.getDisplayedMessage()).isEqualTo(Messages.SO_START);
+            assertThat(device.dispenseGumball()).isFalse();
+        }
+    }
 }
